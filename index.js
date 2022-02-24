eb2c89f75a151dd9acee304c55570bf73179fa7e
Ricardo Correa Ribeiro <ricardo.ribeiro@compasso.com.br>
2022-02-24 17:39:24 -0300

    configuração no get

--- index.js
+++ index.js
@@ -3,12 +3,23 @@ const app = express()
 
 const port = process.env.PORT || 3000
 
-let db = "Tudo OK no Teste 40%"
+let db = ""
+let novopedidopizza = "Não";
+let totalDepedido = 0;
+let totalDeConsume = 0;
+let opcao = ""
 
-let inbenta = {
-    "status": "success",
-    "chatbot_response": db
-    
+
+if(novopedidopizza == 'Sim'){
+    db = "Novo Pedido"
+    totalDepedido = 2
+    totalDeConsume = 40
+    opcao = novopedidopizza
+}else {
+    db = "Escolha Bebida"
+    totalDepedido = 1
+    totalDeConsume = 20
+    opcao = novopedidopizza
 }
 
 //buscar dadoss
@@ -21,8 +32,28 @@ app.post('/teste', (req, res) => {
 
     res.send(JSON.stringify( inbenta ))
     return
-}),
+})
 
+
+let inbenta = {
+    "status": "success",
+    "chatbot_response": db,
+    "raw_output": [
+        {
+            "output_variable": "totalDepedido",
+            "output_result": totalDepedido
+        },
+        {
+            "output_variable": "totalDeConsume",
+            "output_result":  totalDeConsume
+        },
+        {
+            "output_variable": "opcao",
+            "output_result": opcao
+        }
+    ]
+    
+}
 
 app.listen(port, () => {
     console.log('Teste API no Heroku')
