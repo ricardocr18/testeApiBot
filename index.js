1e4ccc34aa521593c353756705f3cba912ec0c03
Ricardo Correa Ribeiro <ricardo.ribeiro@compasso.com.br>
2022-02-24 10:25:54 -0300

    mudança no index.js

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Teste de API no Heroku!!!')
+    res.send('Teste de API no Heroku GET !!!')
+
+    
 })
+
+app.post('/apipost', (req, res) => {
+    res.send('Teste de API no Heroku POST!!!')
+
+    
+})
+
 
 app.listen(port, () => {
     console.log('Teste API no Heroku')
