546af302e5f40a8dcbf1e2de851ee073c7fc7cfa
Ricardo Correa Ribeiro <ricardo.ribeiro@compasso.com.br>
2022-03-02 15:26:20 -0300

    configuração na API

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ app.use(express.urlencoded({ extended: true })) // Aqui eu pego as informsaçõe
 const port = process.env.PORT || 3000
 
 let db = ""
-    let novopedidopizza = "";
-    let totalDepedido = 0;
-    let totalDeConsume = 0;
-    let opcao = ""
+let novopedidopizza = "";
+let totalDepedido = 0;
+let totalDeConsume = 0;
+let opcao = ""
 
-    
+
 // if (novopedidopizza == 'Sim') {
 //     db = "Novo Pedido"
 //     totalDepedido = 2
@@ -35,38 +35,40 @@ app.get('/', (req, res) => {
 //inserir dados
 app.post('/teste', (req, res) => {
     console.log("Aqui é uma variavel: " + (req.body.opcao_pizza))
-    res.send(JSON.stringify(inbenta))  
-
-    if(req.body.opcao_pizza = "Camarão"){
-        totalDepedido = 1
-        totalDeConsume = 1
-        opcao = req.body.opcao_pizza
-    } else {
-        totalDepedido = 1
-        totalDeConsume = 1
-        opcao = req.body.opcao_pizza
-    }
-
-    let inbenta = {
-        "status": "success",
-        "chatbot_response": db,
-        "raw_output": [
-            {
-                "output_variable": "totalDepedido",
-                "output_result": totalDepedido
-            },
-            {
-                "output_variable": "totalDeConsume",
-                "output_result": totalDeConsume
-            },
-            {
-                "output_variable": "opcao",
-                "output_result": opcao
-            }
-        ]
-    }
-      
+    res.send(JSON.stringify(inbenta), correa()) 
+    
+    function correa () {
+        if (req.body.opcao_pizza = "Camarão") {
+            totalDepedido = 1
+            totalDeConsume = 1
+            opcao = req.body.opcao_pizza
+        } else {
+            totalDepedido = 1
+            totalDeConsume = 1
+            opcao = req.body.opcao_pizza
+        }
+        
+        let inbenta = { 
+            "status": "success",
+            "chatbot_response": db,
+            "raw_output": [
+                {
+                    "output_variable": "totalDepedido",
+                    "output_result": totalDepedido
+                },
+                {
+                    "output_variable": "totalDeConsume",
+                    "output_result": totalDeConsume
+                },
+                {
+                    "output_variable": "opcao",
+                    "output_result": opcao
+                }
+            ]
+        }        
+    } 
 })
+
 
 
 
