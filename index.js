130640af51c18f7be885f75502c868aa5772f80c
Ricardo Correa Ribeiro <ricardo.ribeiro@compasso.com.br>
2022-03-02 18:14:50 -0300

    configuração na API

--- index.js
+++ index.js
@@ -9,23 +9,23 @@ app.use(express.urlencoded({ extended: true })) // Aqui eu pego as informsaçõe
 
 const port = process.env.PORT || 3000
 
-let db = ""
-let novopedidopizza = "";
-let totalDepedido = 0;
-let totalDeConsume = 0;
-let opcao = ""
+// let db = ""
+// let novopedidopizza = "";
+// let totalDepedido = 0;
+// let totalDeConsume = 0;
+// let opcao = ""
 
 
-if (novopedidopizza == 'Sim') {
-    db = "Novo Pedido"
-    totalDepedido = 2
-    totalDeConsume = 40
-    opcao = novopedidopizza
-} else {
-    db = "Bora API"   
-    totalDepedido = 1
-    totalDeConsume = 20    
-}
+// if (novopedidopizza == 'Sim') {
+//     db = "Novo Pedido"
+//     totalDepedido = 2
+//     totalDeConsume = 40
+//     opcao = novopedidopizza
+// } else {
+//     db = "Bora API"   
+//     totalDepedido = 1
+//     totalDeConsume = 20    
+// }
 
 //buscar dadoss
 app.get('/', (req, res) => {
@@ -34,8 +34,8 @@ app.get('/', (req, res) => {
 
 //inserir dados
 app.post('/teste', (req, res) => {
-    console.log("Aqui é uma variavel: " + (req.body.opcao_pizza))
-     
+    console.log(req.body)
+    //console.log("Aqui é uma variavel: " + (req.body.opcao_pizza))     
 
     opcao = (req.body.opcao_pizza)
 
@@ -60,25 +60,6 @@ app.post('/teste', (req, res) => {
     
     res.send(JSON.stringify(inbenta))
 })
-
-// let inbenta = { 
-//     "status": "success",
-//     "chatbot_response": db,
-//     "raw_output": [
-//         {
-//             "output_variable": "totalDepedido",
-//             "output_result": totalDepedido
-//         },
-//         {
-//             "output_variable": "totalDeConsume",
-//             "output_result": totalDeConsume
-//         },
-//         {
-//             "output_variable": "opcao",
-//             "output_result": opcao
-//         }
-//     ]
-// }        
 
 app.listen(port, () => {
     console.log('Teste API no Heroku')
