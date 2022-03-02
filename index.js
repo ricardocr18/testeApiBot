a89dfb5b923a66d73660ce9a9f7723e30aa3fe0a
Ricardo Correa Ribeiro <ricardo.ribeiro@compasso.com.br>
2022-03-02 18:17:33 -0300

    configuração na API

--- index.js
+++ index.js
@@ -35,31 +35,29 @@ app.get('/', (req, res) => {
 //inserir dados
 app.post('/teste', (req, res) => {
     console.log(req.body)
-    //console.log("Aqui é uma variavel: " + (req.body.opcao_pizza))     
-
-    opcao = (req.body.opcao_pizza)
-
-    let inbenta = { 
-        "status": "success",
-        "chatbot_response": db,
-        "raw_output": [
-            {
-                "output_variable": "totalDepedido",
-                "output_result": totalDepedido
-            },
-            {
-                "output_variable": "totalDeConsume",
-                "output_result": totalDeConsume
-            },
-            {
-                "output_variable": "opcao",
-                "output_result": opcao
-            }
-        ]
-    }  
+    //console.log("Aqui é uma variavel: " + (req.body.opcao_pizza))      
     
     res.send(JSON.stringify(inbenta))
 })
+
+let inbenta = { 
+    "status": "success",
+    "chatbot_response": db,
+    "raw_output": [
+        {
+            "output_variable": "totalDepedido",
+            "output_result": totalDepedido
+        },
+        {
+            "output_variable": "totalDeConsume",
+            "output_result": totalDeConsume
+        },
+        {
+            "output_variable": "opcao",
+            "output_result": opcao
+        }
+    ]
+}  
 
 app.listen(port, () => {
     console.log('Teste API no Heroku')
